fdb20b561e122349174af5681fd8d386f23a83a1
x72y5e <x72y5e@gmail.com>
2019-04-30 21:37:56 +0100

    random selection of available items

--- src/knapsack.rs
+++ src/knapsack.rs
@@ -1,3 +1,8 @@
+extern crate rand;
+
+use rand::{Rng, thread_rng};
+use rand::seq::SliceRandom;
+
 
 #[derive(Clone, Debug)]
 struct Item {
@@ -18,9 +23,7 @@ impl<'a> Knapsack<'a> {
     }
 }
 
-fn make_table(items: &Vec<Item>) -> Knapsack {
-    let band = 50;
-    let capacity = 750;
+fn make_table(items: &Vec<Item>, capacity: usize, band_size: usize) -> Knapsack {
 
     let mut table: Vec<Vec<Knapsack>> = vec![];
     let mut optimal = Knapsack::empty();
@@ -34,8 +37,8 @@ fn make_table(items: &Vec<Item>) -> Knapsack {
         let value = item.value;
 
         // for each object, cycle through weight bands
-        for j in 0..((capacity / band)) {
-            let weight_limit = (j + 1) * band;
+        for j in 0..((capacity / band_size)) {
+            let weight_limit = (j + 1) * band_size;
 
             // get previous best knapsack for this weight
             let prev_max = match i {
@@ -51,8 +54,8 @@ fn make_table(items: &Vec<Item>) -> Knapsack {
                 // calculate value of remaining space
                 let weight_remaining = weight_limit - weight;
                 let sub_knapsack = {
-                    if weight_remaining >= band {
-                        let weight_column = (weight_remaining / band) - 1;
+                    if weight_remaining >= band_size {
+                        let weight_column = (weight_remaining / band_size) - 1;
                         match i {
                             0 => Knapsack::empty(),
                             _ => table[i - 1][weight_column].clone(), // max from prev row for weight available
@@ -90,19 +93,30 @@ fn make_table(items: &Vec<Item>) -> Knapsack {
         table.push(row);
 
     }
-
-    for r in table.iter() {
-        println!("{:?}", r);
-    }
     optimal
 }
 
-pub fn run() {
+fn generate_items(n: usize) -> Vec<Item> {
     let lemon = Item { name: "lemon".to_string(), weight: 50, value: 1} ;
     let sugar = Item {name: "sugar".to_string(), weight:500, value: 2} ;
     let book = Item {name: "book".to_string(), weight: 150, value: 6} ;
     let wine = Item {name: "wine".to_string(), weight: 400, value: 12 } ;
     let all_items = vec![lemon, sugar, book, wine];
-    let optimal = make_table(&all_items);
-    println!("optimal: {:?}", optimal);
+    let mut rng = thread_rng();
+    let sampled: Vec<Item> = (0..n).map(|_|
+        all_items[rng.gen_range(0, all_items.len())].clone())
+        .collect();
+    sampled
+}
+
+pub fn run() {
+    let random_items = generate_items(20);
+    for item in &random_items {
+        println!("{:?}", item);
+    }
+
+    let optimal = make_table(&random_items, 1750, 50);
+    for item in optimal.contents.unwrap() {
+        println!("optimal: {:?}", item);
+    }
 }
